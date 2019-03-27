73302be8ec3785d742ec224729cfb99f81a47b19
Eduardo Straub <eduardo.straub1@gmail.com>
2019-03-27 17:58:15 -0300

    Se agregan tareas, falta las imagenes de las tareas.

--- to_do_18/db/seeds.rb
+++ to_do_18/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: 'Comer empanadas', image: 'https://images.media-allrecipes.com/userphotos/720x405/3715675.jpg')
+Task.create(name: 'Ir a una fonda', image: '')
+Task.create(name: 'Comer anticucho', image: '')
+Task.create(name: 'Tomarse un terremoto', image: '')
+Task.create(name: 'Bailar una cueca', image: '')
+Task.create(name: 'Elevar un volantin', image: '')
+Task.create(name: 'Jugar una pichanga', image: '')
+Task.create(name: 'Comer asado', image: '')
+Task.create(name: 'Tomar chicha', image: '')
